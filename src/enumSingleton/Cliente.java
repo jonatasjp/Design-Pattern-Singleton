9d1471ddfb273e48a9ef3561c559f4ab21717e02
Jonatas Luis <jonatasluis.unipe@gmail.com>
2017-11-08 21:30:43 -0200

    finalização singleton

--- src/enumSingleton/Cliente.java
+++ src/enumSingleton/Cliente.java
@@ -1,5 +1,21 @@
 package enumSingleton;
 
 public class Cliente {
+	public static void main(String[] args) {
+		 /*(2) Obtém instância do FileLogger Ao acessar pela primeira vez o
+		 campo estático INSTANCE de Filelloger,
+		 a classe Filelloger será carregada e inicializada pela JVM. Este
+		 processo inicializa o campo estático INSTANCE uma vez, de forma
+		 preguiçosa!*/
+		FileLogger logger = FileLogger.INSTANCE;
+		logger.log("Uma mensagem para registrar");
 
+		FileLogger logger02 = FileLogger.INSTANCE;
+		if (logger == logger02) {
+			System.out.println("Os objetos logger e logger02 são a mesma instância");
+		} else {
+			System.out.println("O padrão Singleton falhou!!!");
+		}
+
+	}
 }
--- src/enumSingleton/FileLogger.java
+++ src/enumSingleton/FileLogger.java
@@ -1,12 +1,11 @@
 package enumSingleton;
 
 public enum FileLogger {
-	// (1) Cria apenas o campo estático INSTANCE em "Tempo de Compilação"
+	// Cria apenas o campo estático INSTANCE em "Tempo de Compilação"
 	// O objeto FileLogger só será criado na JVM no seu primeiro acesso!
 	INSTANCE;
+	
 	public void log(String msg) {
-		// Simula código para fazer o log de mensagens
 		System.out.println("Registrado no Log: " + msg);
 	}
-	// Outros métodos e váriáveis de instância próprios do FileLogger
 }
